f9ccbabfb95063bfd4ca382e16c511a99e3a599c
mdguggenbichler <michael@mdg-labs.dev>
2026-08-02 05:49:47 +0200

    training: code comments

--- src/playground/temperature.ts
+++ src/playground/temperature.ts
@@ -1,7 +1,9 @@
+// convert celsius to fahrenheit
 export function celsiusToFahrenheit(celsius: number): number {
     return celsius * 9 / 5 + 32 
 }
 
+// convert seperated naming format to one full name
 export function formatFullName(first: string, last: string) {
     return `${last}, ${first}`
 }
